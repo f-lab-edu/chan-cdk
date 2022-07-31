c356c98cfd9944dff0b3d0179ab936b782b96420
revino <lil_w@naver.com>
2022-07-31 20:16:01 +0900

    update repository config
    repository config update

--- config/repositoryConfig.ts
+++ config/repositoryConfig.ts
@@ -6,23 +6,29 @@ export type GitRepo = {
 }
 
 export const INFRA_GIT_REPO:GitRepo = { 
-  owner: 'revino',
-  repoName: 'chan_ias_test', 
-  branch: 'main',
+  owner: 'f-lab-edu',
+  repoName: 'chan-cdk', 
+  branch: 'master',
   tokenName: 'github-token',
 };
 
 export const CUSTOMER_GIT_REPO:GitRepo  = { 
-  owner: 'revino',
-  repoName: 'Chan-Customer', 
-  branch: 'main',
+  owner: 'f-lab-edu',
+  repoName: 'chan-customer', 
+  branch: 'master',
   tokenName: 'github-token',
 };
 
 export const SELLER_GIT_REPO:GitRepo  = { 
-  owner: 'Tloz-z',
-  repoName: 'chan_seller', 
+  owner: 'f-lab-edu',
+  repoName: 'chan-seller', 
   branch: 'main',
   tokenName: 'github-token',
 };
 
+export const LOGISTICS_GIT_REPO:GitRepo  = { 
+  owner: 'f-lab-edu',
+  repoName: 'chan-logistics', 
+  branch: 'main',
+  tokenName: 'github-token',
+};
